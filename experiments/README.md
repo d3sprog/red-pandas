2b04be3c103da98945f3d728b332b22080bdd63d
Tomas Petricek <tomas@tomasp.net>
2024-02-10 00:44:01 +0100

    Random experiments and remarks

---
+++ experiments/README.md
@@ -0,0 +1,13 @@
+Install the following:
+
+```
+pip install jupyterlab
+pip install pandas
+pip install pyarrow
+```
+
+Run JupyterLab:
+
+```
+jupyter lab
+```
---
+++ typescript/data.ts
@@ -0,0 +1,31 @@
+interface Titanic {
+  name: string,
+  survived: boolean
+}
+
+let tn : Titanic[] = [
+  { name:"Joe", survived:false },
+  { name:"Jane", survived:true },
+]
+
+// "drops a column from a data frame"
+// (Omit is defined in the TypeScript library - this can be expanded to something)
+function drop<T, K extends string>(data:T[], key:K) : Omit<T, K>[] {
+  throw "not implemented"
+}
+
+drop(tn, "survived")[0].name
+drop(tn, "survived")[0].survived // ERROR
+
+
+// "rename column"
+// (can this be extended to take a record of renamings? no idea...)
+function rename1<T, K1 extends string, K2 extends string>
+    (data:T[], k1:K1, k2:K2) : 
+      { [P in keyof T as (P extends K1 ? K2 : P)]: T[P] }[] {
+  throw "not implemented"
+}
+
+rename1(tn, "survived", "lived")[0].name
+rename1(tn, "survived", "lived")[0].lived
+rename1(tn, "survived", "lived")[0].survived // ERROR
