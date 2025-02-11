f1aaa6b24f82b0fc3b1b3aba41211dc04cd973bb
zenmaya <maya@zenmaya.xyz>
2025-02-11 17:00:01 +0100

    Add from_string parsing

--- constraint-prototype/src/main/scala/Python.scala
+++ constraint-prototype/src/main/scala/Python.scala
@@ -1,5 +1,7 @@
 package red_pandas
 
+import scala.util.matching.Regex
+
 case class PythonEnvironment() {}
 
 def eval_python(code: String, env: PythonEnvironment): Boolean = {
@@ -20,7 +22,32 @@ final class PseudoVariable(name: String) {
     override def toString(): String = "pseudo/" ++ this.name
 }
 
-final case class PythonPredicate(args: List[Term], template: (List[String]) => String, env: PythonEnvironment) extends Substitutable, Foreign {
+final case class PythonPredicate(args: List[Term], template: (List[PythonEvaluatable]) => String, env: PythonEnvironment) extends Substitutable, Foreign {
+  /// parses a python code that contains `$variable`
+  /// for variables that should be substituted by
+  /// the solver
+  /// 
+  /// Some invariants must hold:
+  /// - variables must only contain [a-zA-Z]
+  /// - variables must be unique
+  /// - order of first appearance is the order of the arguments
+  def from_string(string: String, env: PythonEnvironment): PythonPredicate = {
+    val regex = Regex("\\$(\\w+)");
+    val variables = regex.findAllIn(string).distinct.toList
+
+    val template = (args: List[PythonEvaluatable]) => {
+        var code = string
+        
+        for ((variable, i) <- variables.zipWithIndex) {
+            code = code.replaceAllLiterally("$" ++ variable, args(i).python_representation())
+        }
+        code
+    }
+
+    val free_variables = variables.map(Variable(_));
+    PythonPredicate(free_variables, template, env)
+  }
+
   override def substitute_self(substitution: Substitution): Term = {
     this.copy(args = this.args.map(_ match {
         case arg: Variable => transitive_get(substitution, arg)
@@ -29,7 +56,7 @@ final case class PythonPredicate(args: List[Term], template: (List[String]) => S
   }
 
   override def call(): Boolean = if this.args.forall(_.isInstanceOf[PythonEvaluatable]) then {
-    val python_args = this.args.map(_.asInstanceOf[PythonEvaluatable].python_representation())
+    val python_args = this.args.map(_.asInstanceOf[PythonEvaluatable])
     val code = this.template(python_args)
     eval_python(code, this.env)
   } else {
